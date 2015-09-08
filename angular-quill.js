75c79f182bc94201cb776603b33d4b8874e0c2a1
orion <orion@vagrant.home>
2015-09-08 08:03:21 -0700

    Update for pl

--- angular-quill.js
+++ angular-quill.js
@@ -1,3 +1,52 @@
+(function loadQuillPlaceholderModule() {
+    // Custom module to add placeholder text to Quill editor
+    var Placeholder = function(quill, options) {
+        this.quill    = quill;
+        this.options  = options;
+        this.text     = options.text  || "";
+        this.style    = options.style || { 'color': '#a9a9a9' };
+
+        var handler   = this.placeholderHandler.bind(this);
+
+        quill.on("selection-change", handler);
+        quill.placeholder = this.text;
+    };
+
+    Placeholder.prototype.isEmpty = function isEmpty() {
+        // NB: We consider a text-editor containing only the placeholder text to be empty 
+        var length      = this.quill.getLength(),
+            currentText = this.quill.getText(),
+            result      = (length === 1) || (currentText === this.text+"\n");
+        return result;
+    };
+
+    Placeholder.prototype.addPlaceholder = function addPlaceholder() {
+        var placeholder = this.text;
+        
+        this.quill.setText(placeholder + "\n");
+        this.quill.formatText(0, placeholder.length, this.style);
+    };
+
+    Placeholder.prototype.removePlaceholder = function removePlaceholder() {
+        this.quill.setText("\n");
+    };
+
+    Placeholder.prototype.placeholderHandler = function placeholderHandler(range) {
+        if (!range) {
+            // "focus-out"
+            if (this.isEmpty()) this.addPlaceholder();
+        }
+        else {
+            // "focus-in"
+            if (this.isEmpty()) this.removePlaceholder();
+        }
+    };
+
+    Placeholder.prototype.initialize = Placeholder.prototype.placeholderHandler;
+
+    Quill.registerModule('placeholder', Placeholder);
+})();
+
 (function () {
   'use strict';
 
@@ -10,6 +59,7 @@
    *      quill: pass as a string
    *
    */
+
 
   var scripts = document.getElementsByTagName("script"),
       currentScriptPath = scripts[scripts.length-1].src;
@@ -35,7 +85,8 @@
               modules: {
                 'toolbar': { container: '.toolbar' },
                 'image-tooltip': true,
-                'link-tooltip': true
+                'link-tooltip': true,
+                'placeholder': { text: attrs.placeholder || '' }
               },
               theme: 'snow'
             },
